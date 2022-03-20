94e9dc18e2b6c0646b8de761861b45ef2ab29bf3
vladyslav.usichenko <usichenko.vlad67@gmail.com>
2022-03-20 17:14:54 +0200

    comment file logger handler

--- utils.py
+++ utils.py
@@ -10,18 +10,18 @@ def set_logger(name: str) -> logging.Logger:
 
     # Create handlers
     c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler(ROOT_DIR + '/logs/logs.log')
+    # f_handler = logging.FileHandler(ROOT_DIR + '/logs/logs.log')
     c_handler.setLevel(logging.DEBUG)
-    f_handler.setLevel(logging.DEBUG)
+    # f_handler.setLevel(logging.DEBUG)
 
     # Create formatters and add it to handlers
     c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
     f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     c_handler.setFormatter(c_format)
-    f_handler.setFormatter(f_format)
+    # f_handler.setFormatter(f_format)
 
     # Add handlers to the logger
     logger.addHandler(c_handler)
-    logger.addHandler(f_handler)
+    # logger.addHandler(f_handler)
 
     return logger
